ae707e6e30aee51a1d0d1c11c150eb4b786e38b4
Marco Xu <106225683+marcoxulin@users.noreply.github.com>
2023-04-28 08:10:54 -0700

    semestersRequired correct solution

--- graphs/semestersRequired.js
+++ graphs/semestersRequired.js
@@ -1,42 +1,39 @@
 const semestersRequired = (numCourses, prereqs) => {
-    const graph = {}
-    prereqs.forEach(course => {
-      if(!(course[0] in graph)) graph[course[0]] = [];
-      if(!(course[1] in graph)) graph[course[1]] = [];
-      graph[course[0]].push(course[1]);
-    });
-    const distance = {};
-    for(const course in graph){
-      if(graph[course].length === 0) distance[course] = 1;
-    }
-    for(const course in graph){
-      countSemester(course, graph, distance)
-    }
-    console.log(distance)
-    return Math.max(...Object.values(distance));
-  };
-  
-  function countSemester (node, graph, distance){
-    if(node in distance) return distance[node];
-    let maxDistance = 0;
-    for(let neighbor of graph[node]){
-      const neighborDistance = countSemester(neighbor, graph, distance);
-      if (neighborDistance > maxDistance) maxDistance = neighborDistance;
-    }
-    distance[node] = 1 + maxDistance;
-    return distance[node];
+  const graph = {};
+  for(let i = 0; i < numCourses; i++){
+    graph[i] = [];
   }
+  for(const course of prereqs){
+    const [a,b] = course;
+    graph[a].push(b);
+  }
+  const distance = {};
+  //find ending courses
+  for(const course in graph){
+    if(graph[course].length === 0) {
+      distance[course] = 1;
+    }
+  }
+  for(const course in graph){
+    traverse(course, distance, graph)
+  }
+  return Math.max(...Object.values(distance));
+};
+
+function traverse (node, distance, graph){
+  if(node in distance) return distance[node];
   
-  module.exports = {
-    semestersRequired,
-  };
-  
-  const numCourses = 6;
-  const prereqs = [
-    [1, 2],
-    [2, 4],
-    [3, 5],
-    [0, 5],
-  ];
-  console.log(semestersRequired(numCourses, prereqs)); 
-  
+  let maxDistance = 0;
+  for(let neighbor of graph[node]){
+    const currDistance = traverse(neighbor, distance, graph);
+    if(currDistance > maxDistance) maxDistance = currDistance;
+  }
+  //one distance away from the neighbor node
+  distance[node] = 1 + maxDistance
+  return distance[node];
+}
+
+module.exports = {
+  semestersRequired,
+};
+
